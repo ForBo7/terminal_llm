ae978fca4da58dab34db4d225b24e779d283582f
Salman Naqvi <salmanaqvi+github@icloud.com>
2024-04-12 17:55:30 +0800

    Created dummy methods.

--- terminal_llm/interface.py
+++ terminal_llm/interface.py
@@ -9,12 +9,52 @@ from .chat import Chat
 # %% ../nbs/01_interface.ipynb 4
 class UserInterface:
     def __init__(self, api_key, model): self.chat = Chat(api_key, model)
-        
-    def start_chat(self):
+    
+    def start_app(self): pass
+
+    def menu(self):
+      # TODO: Implement first time check.
       print("Welcome to the chat interface. Type 'quit' to exit.")
+      print("0 – Settings")
+      print("1 – Start Chat")
+      print("2 – Previous Chats")
+      print("3 – Exit")
+
       while True:
-        user_input = input("You: ")
+        choice = input('... ')
+        match choice:
+          case '0': self.settings()
+          case '1': self.start_chat()
+          case '2': self.previous_chats()
+          case '3': break
+          case  _ : print('Invalid choice.')
+                
         # TODO: Implement full processing on exit term.
-        if user_input.lower() == 'quit': break
-        response = self.chat(user_input)
-        print(f"Assistant: {response}")
+        # if user_input.lower() == 'quit': break
+        # response = self.chat(user_input)
+        # print(f"Assistant: {response}")
+
+    def settings(self):
+        print("Settings")
+        print("0 – Change API Key")
+        print("1 – Change Model")
+        print("2 – Back")
+        choice = input('... ')
+        match choice:
+          case '0': self.change_api_key()
+          case '1': self.change_model()
+          case '2': pass
+          case  _ : print('Invalid choice.')
+    
+    def start_chat(self):
+        print("Start Chat")
+        while True:
+          user_input = input('... ')
+          if user_input.lower() == 'quit': break
+          response = self.chat(user_input)
+          print(f"Assistant: {response}")
+    
+    def previous_chats(self):
+        print("Previous Chats")
+        pass
+
